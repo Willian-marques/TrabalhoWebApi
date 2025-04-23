ceefcbf4cbc5b5e14ec66d5de97b49498dc19c30
NPJ Matheus <matheusnpj@outlook.com>
2025-04-22 21:14:37 -0300

    Iniciação do CRUD

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -10,35 +10,43 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var tarefas = new List<object>();
 
-app.MapGet("/weatherforecast", () =>
+//Listando todas as tarefas
+app.MapGet("/api/tarefas", () => tarefas);
+
+//Buscando uma tarefa específica
+app.MapGet("/api/tarefas/{id}", (int id) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+  var tarefa = tarefas.FirstOrDefault(t => ((dynamic)t).Id == id);
+  return tarefa is not null ? Results.Ok(tarefa) : Results.NotFound();
+});
+
+
+//Criando a rota via POST para criar a tarefa ramdomicamente
+app.MapPost("/api/tarefas", () =>
+{
+
+  //Estrutura de dado TAREFA.
+  var tarefa = new
+  {
+    Id = new Random().Next(1, 1000),
+    Titulo = "Tarefa" + new Random().Next(1, 100),
+    Descricao = "Descrição aleatória da tarefa",
+    Concluida = new Random().Next(0, 2) == 1,
+    DataCriacao = DateTime.Now
+  };
+
+  //Adiciona o objeto TAREFA a lista tarefaS
+  tarefas.Add(tarefa);
+  return Results.Created($"/api/tarefas/{tarefa.Id}", tarefa);
+
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
